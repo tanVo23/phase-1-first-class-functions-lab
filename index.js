978e53e37ebe11987e7212ddae3115762abdc652
Tan <tan10tvo@gmail.com>
2021-06-04 14:48:24 -0600

    commit tree 20916b02602e3c401cc50e377d128a3fb720af69

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers= function(driversArray){
+    const selectingDrivers= driversArray.slice(0,2)
+    return selectingDrivers;
+    }
+    const returnLastTwoDrivers= function(driversArray){
+    const selectingDrivers= driversArray.slice(driversArray.length-2,driversArray.length)
+    return selectingDrivers;
+    }
+    const selectingDrivers= [returnFirstTwoDrivers, returnLastTwoDrivers];
+    function createFareMultiplier(num){
+    return function(fare){
+    return fare*num;
+    }
+    }
+    const fareDoubler= createFareMultiplier(2);
+    const fareTripler= createFareMultiplier(3);
+    function selectDifferentDrivers(driversArray, func){
+    return func(driversArray);
+    }
